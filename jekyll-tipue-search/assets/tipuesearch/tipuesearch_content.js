f15a04b67ec502bdd9a19b6a6783990bad71de32
Greg Summers <Gregory.Summers@ibm.com>
2022-06-07 10:07:14 -0700

    results pagination

--- jekyll-tipue-search/assets/tipuesearch/tipuesearch_content.js
+++ jekyll-tipue-search/assets/tipuesearch/tipuesearch_content.js
@@ -4,63 +4,59 @@ var tipuesearch = {"pages": [{
   "tags": "central home main_page",
   "url": "index.html"
 },{
-  "title": "APIs",
-  "text": "Use z/OS Connect EE to create APIs in minutes to access IBM Z applications and data..",
+  "title": "APIs <span class=\"search-link\">LEARN MORE</span>",
+  "text": "Use z/OS Connect EE to create APIs in minutes to access IBM Z applications and data.",
   "tags": "api apis IMS-apis",
   "url": "ims-apis.html"
 },{
-  "title": "Automation",
+  "title": "Automation <span class=\"search-link\">LEARN MORE</span>",
   "text": "Learn how to automate your IBM Z IT infrastructure and applications by using Ansible.",
   "tags": "automation ansible",
   "url": "ims-devops.html"
 },{
-  "title": "Java <span class=\"search-link\">MORE?</span>",
-  "text": "Build Java applications to run inside or outside IMS..",
-  "tags": "loremipsum",
-  "url": "imsdev.github.io/ims-java.html"
+  "title": "Java <span class=\"search-link\">LEARN MORE</span>",
+  "text": "Build Java applications to run inside or outside IMS.",
+  "tags": "Java java_solution",
+  "url": "ims-java.html"
 },{
-  "title": "IMS Gold",
-  "text": "Bacon ipsum dolor sit amet flank sunt jerky dolore beef. Ut aute ham hock hamburger pork loin dolor esse ground round tenderloin consequat chicken veniam enim. Dolore kielbasa kevin andouille flank id consectetur chicken voluptate tenderloin. Jerky beef ribs tri-tip, kielbasa tail corned beef non. Sausage id ex kielbasa meatloaf sed drumstick ad kevin landjaeger ut. Consequat venison frankfurter, culpa swine aliquip turkey tempor beef. Prosciutto shank boudin leberkas pastrami.",
-  "tags": "loremipsum pork",
-  "url": "/jekyll-tipue-search/loremipsum/2013/12/18/bacon-ipsum-short.html"
+  "title": "IMS Gold <span class=\"search-link\">SIGN UP</span>",
+  "text": "Join the IMS client feedback program for the latest IMS news and engage with other customers.",
+  "tags": "gold ims_gold customer sponsor",
+  "url": "ims-gold.html"
 },{
-  "title": "The Adventure of the Veiled Lodger",
-  "text": "When one considers that Mr. Sherlock Holmes was in active practice for twenty-three years, and that during seventeen of these I was allowed to cooperate with him and to keep notes of his doings, it will be clear that I have a mass of material at my command. The problem has always been not to find but to choose. There is the long row of year-books which fill a shelf, and there are the dispatch-cases filled with documents, a perfect quarry for the student not only of crime but of the social and official scandals of the late Victorian era.",
-  "tags": "literature",
-  "url": "/jekyll-tipue-search/literature/2012/07/24/the-adventure-of-the-veiled-lodger.html"
+  "title": "IMS Makerspace <span class=\"search-link\">SIGN UP</span>",
+  "text": "Sign up to interact with a specialized IMS team who can help you solve real-world business challenges.",
+  "tags": "makerspace ims_makerspace",
+  "url": "ims-makerspace.html"
 },{
-  "title": "The Adventure of the Dying Detective",
-  "text": "Mrs. Hudson, the landlady of Sherlock Holmes, was a long-suffering woman. Not only was her first-floor flat invaded at all hours by throngs of singular and often undesirable characters but her remarkable lodger showed an eccentricity and irregularity in his life which must have sorely tried her patience. ",
-  "tags": "literature",
-  "url": "/jekyll-tipue-search/literature/2012/06/09/the-adventure-of-the-dying-detective.html"
+  "title": "IMS Community <span class=\"search-link\">JOIN</span>",
+  "text": "See the latest IMS blogs, engage in IMS forums, and more by visiting the IMS Community",
+  "tags": "community engage blogs blog",
+  "url": "https://community.ibm.com/community/user/ibmz-and-linuxone/groups/topic-home?CommunityKey=eba3ada3-db89-4dca-9154-328195f5e560"
+
 },{
-  "title": "The Adventure of the Cardboard Box",
-  "text": "In choosing a few typical cases which illustrate the remarkable mental qualities of my friend, Sherlock Holmes, I have endeavoured, as far as possible, to select those which presented the minimum of sensationalism, while offering a fair field for his talents.",
-  "tags": "literature",
-  "url": "/jekyll-tipue-search/literature/2011/11/11/the-adventure-of-the-cardboard-box.html"
+  "title": "IMS Videos <span class=\"search-link\">WATCH</span>",
+  "text": "Choose from hundreds of educational videos on IMS components, functions, and tools.",
+  "tags": "makerspace ims_makerspace",
+  "url": "ims-videos.html"
 },{
-  "title": "Obama Inaugural Address",
-  "text": "My fellow citizens: I stand here today humbled by the task before us, grateful for the trust you have bestowed, mindful of the sacrifices borne by our ancestors. I thank President Bush for his service to our nation, as well as the generosity and cooperation he has shown throughout this transition. Forty-four Americans have now taken the presidential oath.",
-  "tags": "speech",
-  "url": "/jekyll-tipue-search/speeches/2009/01/20/obama-inaugural-address.html"
+  "title": "IMS Courses <span class=\"search-link\">LEARN</span>",
+  "text": "Get in-depth IMS education with these self-paced courses and earn digital badges.",
+  "tags": "learn courses badges ims_courses learn_ims",
+  "url": "ims-education.html"
 },{
-  "title": "Obama's favorite meats",
-  "text": "My fellow citizens: I stand here today humbled by the savory taste of pork, grateful for the meat you have bestowed, mindful of the sacrifices borne by our ancestors. I thank President Bush for his service to our nation, as well as the generosity and cooperation he has shown throughout this transition. Forty-four Americans have now taken the presidential oath.",
-  "tags": "speech meat",
-  "url": "/jekyll-tipue-search/speeches/2009/01/20/obama-addresses-meats.html"
+  "title": "IMS Repos <span class=\"search-link\">BROWSE</span>",
+  "text": "Get code samples and tutorials on IMS components, workflows, Java, and more.",
+  "tags": "repos ims_repos git github",
+  "url": "ims-repos.html"
 },{
-  "title": "America and the Spirit of Volunteerism",
-  "text": "Thank you, all. Please be seated. Welcome to the South Ground of the White House. It is a joy to be here with members of the armies of compassion. I’m really glad you’re here and I appreciate your inspiration to our fellow citizens.",
-  "tags": "speeches",
-  "url": "/jekyll-tipue-search/speeches/2008/09/08/america-and-the-spirit-of-volunteerism-george-w-bush.html"
+  "title": "IMS Documentation <span class=\"search-link\">BROWSE</span>",
+  "text": "Find information on IMS, tools, and other IBM products.",
+  "tags": "docs ims_docs documentation",
+  "url": "ims-docs.html"
 },{
-  "title": "Address to the Nation",
-  "text": "Mr. Speaker, Mr. President Pro Tempore, members of Congress, and fellow Americans: In the normal course of events, Presidents come to this chamber to report on the state of the Union. Tonight, no such report is needed.",
-  "tags": "speeches",
-  "url": "/jekyll-tipue-search/speeches/2001/09/20/address-to-the-nation-george-w-bush.html"
-},{
-  "title": "About",
-  "text": "Demo site for Jekyll Tipue Search This is the base Jekyll theme. You can find out more info about customizing your Jekyll theme, as well as basic Jekyll usage documentation at jekyllrb.com You can find the source code for the Jekyll new theme at: jekyll / minima You can find the source code for Jekyll at jekyll / jekyll",
-  "tags": "",
-  "url": "/jekyll-tipue-search/about/"
+  "title": "IMS Documentation <span class=\"search-link\">BROWSE</span>",
+  "text": "Find information on IMS, tools, and other IBM products.",
+  "tags": "support ims_support ",
+  "url": "ims-support.html"
 }]};

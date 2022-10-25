e263c8a313a6d49f6bf86a2ff3104f88e819ca39
Greg Summers <Gregory.Summers@ibm.com>
2022-10-24 17:15:17 -0700

    search revamp

--- jekyll-tipue-search/assets/tipuesearch/tipuesearch_content.js
+++ jekyll-tipue-search/assets/tipuesearch/tipuesearch_content.js
@@ -1,155 +1,155 @@
 var tipuesearch = {"pages": [{
-  "title": "IMS Central <span class=\"search-link\">HOME</span>",
+  "title": "IMS Central <img class=\"search-icon\" src=\"wp-content/Documentation.svg\"></img>",
   "text": "Build your skills, find documentation, get help, and engage with the IMS team.",
   "tags": "central home main_page",
   "url": "index.html"
 },{
-  "title": "APIs <span class=\"search-link\">LEARN MORE</span>",
+  "title": "APIs <img class=\"search-icon\" src=\"wp-content/Documentation.svg\"></img>",
   "text": "Use z/OS Connect EE to create APIs in minutes to access IBM Z applications and data.",
   "tags": "api apis IMS-apis",
   "url": "ims-apis.html"
 },{
-  "title": "Automation with Ansible <span class=\"search-link\">LEARN MORE</span>",
+  "title": "Automation with Ansible <img class=\"search-icon\" src=\"wp-content/Documentation.svg\"></img>",
   "text": "Learn how to automate your IBM Z IT infrastructure and applications by using Ansible.",
   "tags": "automation ansible",
   "url": "ims-devops.html"
 },{
-  "title": "Java <span class=\"search-link\">LEARN MORE</span>",
+  "title": "Java <img class=\"search-icon\" src=\"wp-content/Documentation.svg\"></img>",
   "text": "Build Java applications to run inside or outside IMS.",
   "tags": "Java java_solution",
   "url": "ims-java.html"
 },{
-  "title": "IMS Gold: Feedback program <span class=\"search-link\">SIGN UP</span>",
+  "title": "IMS Gold: Feedback program <img class=\"search-icon\" src=\"wp-content/Edit.svg\"></img>",
   "text": "Join the IMS client feedback program for the latest IMS news and engage with other customers.",
   "tags": "gold ims_gold customer sponsor",
   "url": "ims-gold.html"
 },{
-  "title": "IMS Makerspace: Collaborate with IMS <span class=\"search-link\">SIGN UP</span>",
+  "title": "IMS Makerspace: Collaborate with IMS <img class=\"search-icon\" src=\"wp-content/Edit.svg\"></img>",
   "text": "Sign up to interact with a specialized IMS team who can help you solve real-world business challenges.",
   "tags": "makerspace ims_makerspace",
   "url": "ims-makerspace.html"
 },{
-  "title": "IMS Community <span class=\"search-link\">JOIN</span>",
-  "text": "See the latest IMS blogs, engage in IMS forums, and more by visiting the IMS Community",
+  "title": "IMS Community <img class=\"search-icon\" src=\"wp-content/Community.svg\"></img>",
+  "text": "See the latest IMS blogs, engage in IMS forums, and more by visiting the IMS Community.",
   "tags": "community engage blogs blog",
   "url": "https://community.ibm.com/community/user/ibmz-and-linuxone/groups/topic-home?CommunityKey=eba3ada3-db89-4dca-9154-328195f5e560"
 },{
-  "title": "IMS Videos <span class=\"search-link\">WATCH</span>",
+  "title": "IMS Videos <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Choose from hundreds of educational videos on IMS components, functions, and tools.",
   "tags": "makerspace ims_makerspace",
   "url": "ims-videos.html"
 },{
-  "title": "IMS Courses <span class=\"search-link\">LEARN</span>",
+  "title": "IMS Courses <img class=\"search-icon\" src=\"wp-content/CapGown.svg\"></img>",
   "text": "Get in-depth IMS education with these self-paced courses and earn digital badges.",
   "tags": "learn courses badges ims_courses learn_ims",
   "url": "ims-education.html"
 },{
-  "title": "IMS Repos <span class=\"search-link\">BROWSE</span>",
+  "title": "IMS Repos <img class=\"search-icon\" src=\"wp-content/Documentation.svg\"></img>",
   "text": "Get code samples and tutorials on IMS components, workflows, Java, and more.",
   "tags": "repos ims_repos git github",
   "url": "ims-repos.html"
 },{
-  "title": "IMS Documentation <span class=\"search-link\">BROWSE</span>",
+  "title": "IMS Documentation <img class=\"search-icon\" src=\"wp-content/Documentation.svg\"></img>",
   "text": "Find information on IMS, tools, and other IBM products. Search among dfs sql abend parameters parm jcl logs",
   "tags": "docs ims_docs documentation dfs sql abend parameters parm jcl logs",
   "url": "ims-docs.html"
 },{
-  "title": "IMS Support <span class=\"search-link\">BROWSE</span>",
+  "title": "IMS Support <img class=\"search-icon\" src=\"wp-content/Edit.svg\"></img>",
   "text": "Find information on IMS, tools, and other IBM products.",
   "tags": "support ims_support ",
   "url": "ims-support.html"
 },{
-  "title": "Introduction to IMS <span class=\"search-link\">WATCH</span>",
+  "title": "Introduction to IMS <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Introduce yourself to the basics of IMS.",
   "tags": "introduction basics ims_basics",
   "url": "ims-videos.html#ims-intro"
 },{
-  "title": "Migrating to IMS 15 <span class=\"search-link\">WATCH</span>",
+  "title": "Migrating to IMS 15 <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Learn how to migrate your IMS to version 15.",
   "tags": "mig migration",
   "url": "ims-videos.html#ims-mig"
 },{
-  "title": "Ansible for IMS <span class=\"search-link\">WATCH</span>",
+  "title": "Ansible for IMS <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Learn how to setup your IMS Ansible control node.",
   "tags": "ansible ims_ansible",
   "url": "ims-videos.html#ims-ansible"
 },{
-  "title": "IMS DBRC <span class=\"search-link\">WATCH</span>",
+  "title": "IMS DBRC <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Learn about IMS DBRC.",
   "tags": "dbrc ims_dbrc",
   "url": "ims-videos.html#ims-dbrc"
 },{
-  "title": "IMS database organization <span class=\"search-link\">WATCH</span>",
+  "title": "IMS database organization <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Learn about how to organize your IMS databases.",
   "tags": "db_organization db organization",
   "url": "ims-videos.html#ims-db-org"
 },{
-  "title": "Intro to IMS Catalog <span class=\"search-link\">WATCH</span>",
+  "title": "Intro to IMS Catalog <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Learn about IMS Catalog.",
   "tags": "ims_catalog catalog",
   "url": "ims-videos.html#ims-cat"
 },{
-  "title": "IMS DRD for MODBLKS <span class=\"search-link\">WATCH</span>",
+  "title": "IMS DRD for MODBLKS <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Learn about IMS DRD for MDBLKS.",
   "tags": "ims_drd drd mdblk",
   "url": "ims-videos.html#ims-drd-mdblk"
 },{
-  "title": "IMS DRD for IMSRSC <span class=\"search-link\">WATCH</span>",
+  "title": "IMS DRD for IMSRSC <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Learn about IMS DRD for IMSRSC.",
   "tags": "ims_drd drd imsrsc",
   "url": "ims-videos.html#ims-drd-imsrsc"
 },{
-  "title": "IMS dynamic commands <span class=\"search-link\">WATCH</span>",
+  "title": "IMS dynamic commands <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Learn about IMS dynamic commands.",
   "tags": "ims cmd cmds ims_commands commands",
   "url": "ims-videos.html#ims-dyn-cmds"
 },{
-  "title": "IMS dynamic commands <span class=\"search-link\">WATCH</span>",
+  "title": "IMS dynamic commands <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Learn about IMS High Availability.",
   "tags": "ims_ha high_availability",
   "url": "ims-videos.html#ims-ha"
 },{
-  "title": "Java in IMS <span class=\"search-link\">WATCH</span>",
+  "title": "Java in IMS <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Learn about Java programming in IMS.",
   "tags": "ims_java java",
   "url": "ims-videos.html#ims-java"
 },{
-  "title": "Database application programming <span class=\"search-link\">WATCH</span>",
+  "title": "Database application programming <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Learn about database application programming in IMS.",
   "tags": "ims_db database_programming drivers",
   "url": "ims-videos.html#ims-db-prog"
 },{
-  "title": "IMS Explorer <span class=\"search-link\">WATCH</span>",
+  "title": "IMS Explorer <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Learn about IMS Explorer for development.",
   "tags": "ims_explorer explorer_for_development development",
   "url": "ims-videos.html#ims-exp"
 },{
-  "title": "IMS Tools <span class=\"search-link\">WATCH</span>",
+  "title": "IMS Tools <img class=\"search-icon\" src=\"wp-content/Video.svg\"></img>",
   "text": "Review videos for IMS Tools.",
   "tags": "ims_tools tools ",
   "url": "ims-videos.html#ims-tools"
 },{
-  "title": "IMS Fundamentals <span class=\"search-link\">LEARN</span>",
+  "title": "IMS Fundamentals <img class=\"search-icon\" src=\"wp-content/CapGown.svg\"></img>",
   "text": "Take an overview of IMS.",
   "tags": "learn courses badges ims_fundamentals",
   "url": "ims-education.html#ims-fundamentals"
 },{
-  "title": "IMS Diagnostics <span class=\"search-link\">LEARN</span>",
+  "title": "IMS Diagnostics <img class=\"search-icon\" src=\"wp-content/CapGown.svg\"></img>",
   "text": "Take an overview of IMS diagnostic tools.",
   "tags": "learn courses badges ims_diagnostics",
   "url": "ims-education.html#ims-diagnostics"
 },{
-  "title": "IMS DBCTL <span class=\"search-link\">LEARN</span>",
+  "title": "IMS DBCTL <img class=\"search-icon\" src=\"wp-content/CapGown.svg\"></img>",
   "text": "Take an overview of IMS database control environment.",
   "tags": "learn courses badges ims_diagnostics",
   "url": "ims-education.html#ims-dbctl"
 },{
-  "title": "IMS shared queues <span class=\"search-link\">LEARN</span>",
+  "title": "IMS shared queues <img class=\"search-icon\" src=\"wp-content/CapGown.svg\"></img>",
   "text": "Learn how shared queues work in IMS.",
   "tags": "learn courses badges ims_shared_queues",
   "url": "ims-education.html#ims-squeues"
 },{
-  "title": "IMS fast path implementation <span class=\"search-link\">LEARN</span>",
+  "title": "IMS fast path implementation <img class=\"search-icon\" src=\"wp-content/CapGown.svg\"></img>",
   "text": "Learn how to implement fast path in IMS.",
   "tags": "learn courses badges ims_fast_path",
   "url": "ims-education.html#ims-fpimpl"
